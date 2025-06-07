28fd1a8ca01e5d78e8fc863d176d0e86e2001868
Mafien_0 <93920918+Mafien01@users.noreply.github.com>
2025-06-07 18:26:58 +0500

    testing rofi configs

--- nixos/modules/rofi.nix
+++ nixos/modules/rofi.nix
@@ -1,13 +1,18 @@
 { config, pkgs, ...}: {
 
   programs.rofi = {
-    enable = true;
-    package = pkgs.rofi-wayland;
-    plugins = with pkgs; [
-      (rofi-calc.override { rofi-unwrapped = rofi-wayland-unwrapped; })
-        (rofi-emoji.override { rofi-unwrapped = rofi-wayland-unwrapped; })
-        (rofi-power-menu.override { rofi-unwrapped = rofi-wayland-unwrapped; })
-    ];
+    #enable = true;
+    #package = pkgs.rofi-wayland;
+    #plugins = with pkgs; [
+    #  (rofi-calc.override { rofi-unwrapped = rofi-wayland-unwrapped; })
+    #    (rofi-emoji.override { rofi-unwrapped = rofi-wayland-unwrapped; })
+    #    (rofi-power-menu.override { rofi-unwrapped = rofi-wayland-unwrapped; })
+    #];
     modes = [ "drun" "run" "window" "calc" "emoji" "power"];
   };
+
+
+  nixpkgs.overlays = [(final: prev: {
+      rofi-calc = prev.rofi-calc.override { rofi-unwrapped = prev.rofi-wayland-unwrapped; };
+  })];
 }
