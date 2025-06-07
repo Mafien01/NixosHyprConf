b40881279855131d8f0558bdd2e809a7ebf546da
Mafien_0 <93920918+Mafien01@users.noreply.github.com>
2025-06-07 18:23:17 +0500

    a

--- nixos/modules/rofi.nix
+++ nixos/modules/rofi.nix
@@ -5,8 +5,9 @@
     package = pkgs.rofi-wayland;
     plugins = with pkgs; [
       (rofi-calc.override { rofi-unwrapped = rofi-wayland-unwrapped; })
-      (rofi-emoji.override { rofi-unwrapped = rofi-wayland-unwrapped; })
-      (rofi-power-menu.override { rofi-unwrapped = rofi-wayland-unwrapped; })
-      ];
+        (rofi-emoji.override { rofi-unwrapped = rofi-wayland-unwrapped; })
+        (rofi-power-menu.override { rofi-unwrapped = rofi-wayland-unwrapped; })
+    ];
     modes = [ "drun" "run" "window" "calc" "emoji" "power"];
-};
+  };
+}
