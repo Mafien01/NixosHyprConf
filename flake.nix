d47d63ebf9effe73c955234abecb91aa79fcca82
Mafien_0 <93920918+Mafien01@users.noreply.github.com>
2025-06-09 10:48:22 +0500

    hyprland plugins

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1&ref=0.49.0";
+    hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1&ref=v0.49.0";
 
     hy3 = {
       url = "github:outfoxxed/hy3?ref=hl0.49.0";
