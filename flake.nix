84d7f48e9c4fe255440ada7a1962031a756adfef
Mafien_0 <93920918+Mafien01@users.noreply.github.com>
2025-06-09 10:40:24 +0500

    hyprland plugins

--- flake.nix
+++ flake.nix
@@ -3,23 +3,39 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
-  
+
     home-manager = {
       url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1&ref=0.49.0"; # Example: pin to a specific version
+
+    hy3 = {
+      url = "github:outfoxxed/hy3?ref=hl0.49.0";
+      inputs.hyprland.follows = "hyprland";
+    };
   };
 
-  outputs = { nixpkgs, home-manager, ... }: {
+  outputs = { nixpkgs, home-manager, hyprland, hy3, ... }: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [ ./nixos/configuration.nix ];
     };
 
     homeConfigurations.mafien0 = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages."x86_64-linux";
-        modules = [ ./home-manager/home.nix ];
-      };
+      pkgs = nixpkgs.legacyPackages."x86_64-linux";
+      modules = [
+        ./home-manager/home.nix
+        hyprland.homeManagerModules.default
 
+        {
+          wayland.windowManager.hyprland = {
+            enable = true;
+            plugins = [ hy3.packages."x86_64-linux".hy3 ];
+          };
+        }
+      ];
+    };
   };
 }
