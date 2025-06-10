2d7d039bb202b2a27b5f24707a48883ba23d803d
Mafien_0 <93920918+Mafien01@users.noreply.github.com>
2025-06-10 18:55:46 +0500

    fixes

--- home-manager/modules/clipboard.nix
+++ home-manager/modules/clipboard.nix
@@ -2,28 +2,28 @@
   systemd.user.enable = true;
 
   systemd.user.services.cliphist-wipe-on-boot = {
-    description = "wipe cliphist on boot";
-    serviceConfig  = {
-          Type = "oneshot";
-          ExecStart = "${lib.getExe pkgs.cliphist} wipe";
+    description = "Wipe cliphist on boot";
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "${lib.getExe pkgs.cliphist} wipe";
     };
   };
 
   systemd.user.timers.cliphist-wipe-on-boot = {
-    description = "timer to wipe cliphist once after boot";
+    description = "Timer to wipe cliphist once after boot";
     install = {
       WantedBy = [ "timers.target" ];
     };
     timerConfig = {
-        OnBootSec = "10s";
-        Persistent = false;
+      OnBootSec = "10s";
+      Persistent = false;
     };
     unit = "cliphist-wipe-on-boot.service";
   };
 
   systemd.user.services.cliphist-session-watcher = {
-    description = "clipboard hystory watcher for current session";
-    after  = { "graphical-session.target" = [ ]; };
+    description = "Clipboard history watcher for current session";
+    after = [ "graphical-session.target" ];
     install = {
       WantedBy = [ "graphical-session.target" ];
     };
@@ -32,13 +32,12 @@
       ExecStart = "${lib.getExe pkgs.wl-clipboard} --type text --watch ${lib.getExe pkgs.cliphist} store";
       Restart = "on-failure";
       RestartSec = 5;
-
     };
   };
 
-  home.packages = with pkgs;
-  [
+  home.packages = with pkgs; [
     cliphist
     wl-clipboard
   ];
 }
+
